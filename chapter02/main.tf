286506d1ef55c5587567102a93a63fb1d2dda390
Pacheco, Cesar (Contractor) <CPacheco@carnival.com>
2019-01-01 22:46:45 -0500

    Added cluster web server and configured ASG + ELB

--- chapter02/main.tf
+++ chapter02/main.tf
@@ -6,32 +6,21 @@ provider "aws" {
 }
 
 ##########################################################################################
-### input variables
-##########################################################################################
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  default = 8080
-}
-
-##########################################################################################
 ### resources
 ##########################################################################################
 resource "aws_launch_configuration" "example" {
-  ami           = "ami-40d28157"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  image_id          = "ami-40d28157"
+  instance_type     = "t2.micro"
+  security_groups   = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
-
-
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
-  image_id = ""
 }
 
 resource "aws_security_group" "instance" {
@@ -43,13 +32,85 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks  = ["0.0.0.0/0"]
   }
+
+    lifecycle {
+    create_before_destroy = true
+  }
 }
 
+resource "aws_security_group" "elb" {
+  name = "terraform-asg-example"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "example" {
+  name                = "terraform-asg-example"
+  availability_zones  = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+
+##########################################################################################
+### data sources
+##########################################################################################
+data  "aws_availability_zones" "all" {}
 
 
 ##########################################################################################
-### Output Variables
+### input variables
+##########################################################################################
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+##########################################################################################
+### output Variables
 ##########################################################################################
 output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+  value = "${aws_elb.example.dns_name}"
 }
