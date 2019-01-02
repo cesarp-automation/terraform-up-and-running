4acf6c817d18c1bd8b0d6e9e16b211aeead1e8dc
Pacheco, Cesar (Contractor) <CPacheco@carnival.com>
2019-01-01 21:58:45 -0500

    Added Input and Output Variables

--- chapter02/main.tf
+++ chapter02/main.tf
@@ -1,8 +1,22 @@
+##########################################################################################
+### providers
+##########################################################################################
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+##########################################################################################
+### input variables
+##########################################################################################
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+##########################################################################################
+### resources
+##########################################################################################
+resource "aws_launch_configuration" "example" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
@@ -10,22 +24,32 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
 
   tags {
     Name = "terraform-example"
   }
+  image_id = ""
 }
 
 resource "aws_security_group" "instance" {
   name = "terraform-instance-example"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks  = ["0.0.0.0/0"]
   }
+}
+
+
+
+##########################################################################################
+### Output Variables
+##########################################################################################
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
